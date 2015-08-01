d66e5b87c11735435c4847b0f614c0df4149a33e
Rafael Gandía Castelló <rafa400@ash.losgandia.com>
2015-08-01 17:48:03 +0200

    UserCake

---
+++ telepi/var/www/usercake.php
@@ -0,0 +1,34 @@
+<?php 
+/*
+UserCake Version: 2.0.1
+http://usercake.com
+*/
+require_once("userCake/config.php");
+if (!securePage($_SERVER['PHP_SELF'])){die();}
+require_once("userCake/models/header.php");
+
+echo "
+<body>
+<div id='wrapper'>
+<div id='top'><div id='logo'></div></div>
+<div id='content'>
+<h1>UserCake</h1>
+<h2>2.00</h2>
+<div id='left-nav'>";
+include("left-nav.php");
+
+echo "
+</div>
+<div id='main'>
+
+<p>
+CONTENT OF YOUR FIRST PAGE
+</p>
+
+</div>
+<div id='bottom'></div>
+</div>
+</body>
+</html>";
+
+?>
