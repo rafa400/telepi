eb00b6ba4bb82dd433613a2a59db2f7604e46fbf
Rafa <rafa400@papa.losgandia.com>
2015-12-30 23:53:06 +0100

    Starting KODI plugin

--- script.telepi-master/addon.py
+++ script.telepi-master/addon.py
@@ -1,11 +1,22 @@
 ﻿import xbmcaddon
 import xbmcgui
+import subprocess,os
  
+def EstoEsUnaFun( str ):
+   xbmcgui.Dialog().ok("ESO ES","AHHHH",str)
+   return
+
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
 line1 = "Hello World!"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
+
  
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+my_setting = addon.getSetting('my_setting') # returns the string 'true' or 'false'
+addon.setSetting('my_setting', 'false')
+
+os.system("echo caca>>/home/rafa400/caca.txt")
+
+xbmcgui.Dialog().ok(addonname, line1, line2, line3 + my_setting)
--- telepi/var/www/userCake/models/db-settings.php
+++ telepi/var/www/userCake/models/db-settings.php
@@ -5,10 +5,10 @@ http://usercake.com
 */
 
 //Database Information
-$db_host = "localhost"; //Host address (most likely localhost)
-$db_name = "202"; //Name of Database
-$db_user = "202"; //Name of database user
-$db_pass = "password"; //Password for database user
+$db_host = "/home/rafa400/MEGA/Comunes/telepi/telepi/var/lib/telepi/telepi.db"; //Host address (most likely localhost)
+$db_name = "telepi"; //Name of Database
+//$db_user = "202"; //Name of database user
+//$db_pass = "password"; //Password for database user
 $db_table_prefix = "uc_";
 
 GLOBAL $errors;
@@ -18,8 +18,16 @@ $errors = array();
 $successes = array();
 
 /* Create a new mysqli object with database connection parameters */
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-GLOBAL $mysqli;
+//$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+class TelepiDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open($db_host);
+    }
+}
+$sqlite=new TelepiDB();
+GLOBAL $sqlite; // $mysqli
 
 if(mysqli_connect_errno()) {
 	echo "Connection Failed: " . mysqli_connect_errno();
